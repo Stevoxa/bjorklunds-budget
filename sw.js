a31095bcba855ad7ed67058500f1465e1e9ff7d0
Stefan Björklund <stefan.bjorklund@gmail.com>
2026-04-01 12:36:05 +0200

    v0.223

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Minimal service worker for offline caching.
    Works with GitHub Pages as long as files are served from the same origin. */
 
-const CACHE_NAME = "bjorklunds-budget-v22";
+const CACHE_NAME = "bjorklunds-budget-v23";
 // Keep app shell URLs stable so file:// also works.
 const ASSETS_TO_CACHE = [
   "./",
